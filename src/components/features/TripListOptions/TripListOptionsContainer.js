d8a6e2c11fd3191640f9de590f6912feeab76b86
Aneta Olbromska <anetaolbromska@gmail.com>
2021-05-27 17:24:09 +0200

    Add subpages & filters

---
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -0,0 +1,19 @@
+import {connect} from 'react-redux';
+import TripListOptions from './TripListOptions';
+import {getAllTags} from '../../../redux/tagsRedux';
+import {getAllFilters, changeSearchPhrase, changeDurationTimeFrom, changeDurationTimeTo, removeTag, addTag} from '../../../redux/filtersRedux';
+
+const mapStateToProps = state => ({
+  tags: getAllTags(state),
+  filters: getAllFilters(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+  addTag: tags => dispatch(addTag(tags)),
+  removeTag: tags => dispatch(removeTag(tags)),
+  changeDurationTimeFrom: duration => dispatch(changeDurationTimeFrom(duration)),
+  changeDurationTimeTo: duration => dispatch(changeDurationTimeTo(duration)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
